37c0171cabe1f529a5d083a38c088705cd026d7f
ElisaHery <elisa.hery@gmail.com>
2020-04-04 19:05:11 +0200

    feat: add possibility of answer leading to external link

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,15 +3,26 @@ import './Button.css';
 
 function Button({ answer, setNextQuestion }) {
   if (!answer) return null;
+  console.log(answer);
 
   return (
     <div className='questionLink'>
-      <button
-        className='button'
-        onClick={() => setNextQuestion(answer.nextQuestion)}
-      >
-        {answer.text}
-      </button>
+      {answer.extLink ? (
+        <a
+          href={'http://' + answer.extLink}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <button className='button'>{answer.text}</button>
+        </a>
+      ) : (
+        <button
+          className='button'
+          onClick={() => setNextQuestion(answer.nextQuestion)}
+        >
+          {answer.text}
+        </button>
+      )}
     </div>
   );
 }
